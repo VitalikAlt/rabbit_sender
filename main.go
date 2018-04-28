c76f52cd6c60e5e262dacc8847da57385a3beac1
VitalikAlt <vitaliksomeman@mail.ru>
2018-04-28 20:05:40 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"channel", // name
+		true,           // durable
+		false,          // delete when unused
+		false,          // exclusive
+		false,          // no-wait
+		nil,            // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// body := `{"task_id":19, "repo": {"project": "product/component1", "host": "http://localhost:10080"}, "platforms": ["linux"], "from":"master"}`
+	// body := `{"task_id": 1, "repo": {"project": "product/component1", "host": "http://localhost:10080"}, "platforms": {"linux":"artifacts"}, "from":"master"}`
+	body := `{"task_id": 5, "repo": {"project": "product/component1", "host": "http://localhost:10080"}, "from": "feature1", "to": "master"}`
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
